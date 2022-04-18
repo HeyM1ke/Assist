c2a57d0938b3723e1460ba71917fa23b7b56c426
Michael <49486947+HeyM1ke@users.noreply.github.com>
2022-04-18 00:54:10 -0400

    Base News Template

---
+++ Assist/Controls/Home/NewsControl.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Assist.Controls.Home.ViewModels;
+
+namespace Assist.Controls.Home
+{
+    /// <summary>
+    /// Interaction logic for NewsControl.xaml
+    /// </summary>
+    public partial class NewsControl : UserControl
+    {
+        private readonly NewsControlViewModel _viewModel;
+        private readonly object _newsData;
+        public NewsControl()
+        {
+            DataContext = _viewModel = new NewsControlViewModel();
+            InitializeComponent();
+        }
+
+        public NewsControl(object newsData)
+        {
+            DataContext = _viewModel = new NewsControlViewModel();
+            InitializeComponent();
+        }
+
+        private async void NewsControl_Loaded(object sender, RoutedEventArgs e)
+        {
+            if (_newsData != null)
+                _viewModel.LoadNews();
+        }
+    }
+}
---
+++ Assist/Controls/Home/ViewModels/NewsControlViewModel.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media.Imaging;
+using Assist.MVVM.ViewModel;
+
+namespace Assist.Controls.Home.ViewModels
+{
+    internal class NewsControlViewModel : ViewModelBase
+    {
+        private string _newsTitle;
+
+        public string NewsTitle
+        {
+            get => _newsTitle;
+            set => SetProperty(ref _newsTitle, value);
+        }
+        private string _newsDescription;
+
+        public string NewsDescription
+        {
+            get => _newsDescription;
+            set => SetProperty(ref _newsDescription, value);
+        }
+
+        private BitmapImage _newsImage;
+        public BitmapImage NewsImage
+        {
+            get => _newsImage;
+            set => SetProperty(ref _newsImage, value);
+        }
+
+        private string _newsUrl;
+
+        public string NewsUrl
+        {
+            get => _newsUrl;
+            set => SetProperty(ref _newsUrl, value);
+        }
+
+
+
+
+
+        public async Task OpenNewsUrl()
+        {
+            Process.Start(new ProcessStartInfo
+            {
+                FileName = NewsUrl,
+                UseShellExecute = true
+            });
+        }
+
+        public async Task LoadNews()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
