c0cd2f311908b052935ec552fa72453289a5179c
Shiick <blizz.shiick@hotmail.com>
2022-12-12 08:25:05 +0100

    Revert "Deathmatch score fix"
    This reverts commit 6a08408815f20d1f1e775d61e1b6d6e504f22a19.

--- Assist/Views/Dashboard/ViewModels/DashboardViewModel.cs
+++ Assist/Views/Dashboard/ViewModels/DashboardViewModel.cs
@@ -263,36 +263,14 @@ namespace Assist.Views.Dashboard.ViewModels
                         // if not 
                         if (ourTeam.Won)
                         {
-                            var mode = match.MatchInformation.QueueID; // get gamemode --Shiick
-                            if (mode.ToLower().Equals("deathmatch")) // if deathmatch --Shiick
-                            {
-                                control.MatchScore = $"{currentP.Stats.Kills} - {currentP.Stats.Kills}";
-                                control.ResultText = Properties.Resources.Profile_WonText;
-                                control.MatchWin = true;
-                            }
-                            else
-                            {
-                                control.MatchScore = $"{ourTeam.RoundsWon} - {notOurTeam.RoundsWon}";
-                                control.ResultText = Properties.Resources.Profile_WonText;
-                                control.MatchWin = true;
-                            }
+                            control.MatchScore = $"{ourTeam.RoundsWon} - {notOurTeam.RoundsWon}";
+                            control.ResultText = Properties.Resources.Profile_WonText;
+                            control.MatchWin = true;
                         }
-                        else
-                        {
-                            var mode = match.MatchInformation.QueueID; // get gamemode --Shiick
-                            if (mode.ToLower().Equals("deathmatch"))  // if deathmatch --Shiick
-                            {
-                                var topPlayerKills = match.Players.Max(player => player.Stats.Kills); // get MVP kill count
-                                control.MatchScore = $"{currentP.Stats.Kills} - {topPlayerKills}";
-                                control.ResultText = Properties.Resources.Profile_LossText;
-                                control.MatchWin = false;
-                            }
-                            else
-                            {
-                                control.MatchScore = $"{currentP.Stats.Kills} - {currentP.Stats.Kills}";
-                                control.ResultText = Properties.Resources.Profile_LossText;
-                                control.MatchWin = false;
-                            }
+                        else{
+                            control.MatchScore = $"{notOurTeam.RoundsWon} - {ourTeam.RoundsWon}";
+                            control.ResultText = Properties.Resources.Profile_LossText;
+                            control.MatchWin = false;
                         }
                 }
 
