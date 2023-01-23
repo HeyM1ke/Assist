5ed7a44cb17ae45afcd467286c2d8285831428c5
Michael <49486947+HeyM1ke@users.noreply.github.com>
2023-01-23 08:12:36 -0500

    Refresh Btn for Lobbies

--- Assist/Game/Views/Lobbies/Pages/BrowserView.axaml.cs
+++ Assist/Game/Views/Lobbies/Pages/BrowserView.axaml.cs
@@ -1,6 +1,9 @@
-﻿using Assist.Game.Views.Lobbies.ViewModels;
+﻿using System;
+using Assist.Game.Views.Lobbies.ViewModels;
 using Avalonia;
 using Avalonia.Controls;
+using Avalonia.Input;
+using Avalonia.Interactivity;
 using Avalonia.Markup.Xaml;
 
 namespace Assist.Game.Views.Lobbies.Pages;
@@ -18,5 +21,35 @@ public partial class BrowserView : UserControl
     private void InitializeComponent()
     {
         AvaloniaXamlLoader.Load(this);
+    }
+
+    private async void BrowserView_Init(object? sender, EventArgs e)
+    {
+        if(Design.IsDesignMode)
+            return;
+
+        await _viewModel.Setup();
+    }
+
+    private async void Refresh_Click(object? sender, RoutedEventArgs e)
+    {
+        var textBox = this.FindControl<TextBox>("SearchBox");
+        textBox.Text = string.Empty;
+
+        await _viewModel.RefreshList();
+    }
+
+    private async void Search_Click(object? sender, RoutedEventArgs e)
+    {
+        var textBox = this.FindControl<TextBox>("SearchBox");
+        await _viewModel.FilterList(textBox.Text);
+    }
+
+    private void SearchBox_OnKeyDown(object? sender, KeyEventArgs e)
+    {
+        if (e.Key == Key.Enter)
+        {
+            Search_Click(null, null);
+        }
     }
 }
--- Assist/Game/Views/Lobbies/ViewModels/BrowserViewModel.cs
+++ Assist/Game/Views/Lobbies/ViewModels/BrowserViewModel.cs
@@ -1,6 +1,8 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using Assist.Game.Controls.Lobbies;
+using Assist.Objects.AssistApi.Game;
 using Assist.ViewModels;
 using ReactiveUI;
 
@@ -16,7 +18,7 @@ public class BrowserViewModel : ViewModelBase
         set => this.RaiseAndSetIfChanged(ref _isLoading, value);
     }
 
-    private List<object> AllLobbiesStorage;
+    private List<AssistLobby> AllLobbiesStorage;
 
     private List<LobbyBrowserPreviewControl> _currentlyShownLobbies;
 
@@ -29,18 +31,72 @@ public class BrowserViewModel : ViewModelBase
     public async Task Setup()
     {
         // Set Loading to True.
-        
-        // Get Users Region
+        IsLoading = true;
+        // Get Users Region LATER
         // Make Request to get Lobbies
         // Store all Lobbies in Storage.
-        
+        AllLobbiesStorage = await AssistApplication.Current.AssistUser.GetAllLobbies();
+
+        List<LobbyBrowserPreviewControl> controls = new List<LobbyBrowserPreviewControl>();
+        for (int i = 0; i < AllLobbiesStorage.Count; i++)
+        {
+            controls.Add(new LobbyBrowserPreviewControl()
+            {
+                LobbyName = AllLobbiesStorage[i].LobbyName,
+                LobbyCode = AllLobbiesStorage[i].Code,
+                CurrentSize = AllLobbiesStorage[i].CurrentPartySize.ToString(),
+                MaxSize = AllLobbiesStorage[i].MaxPartySize.ToString(),
+                IsPasswordProtected = AllLobbiesStorage[i].RequiresPassword
+            });
+        }
+
         // Create Controls and Set them to the Shown list. 
+        CurrentlyShownLobbies = controls;
+        IsLoading = false;
     }
 
-    public async Task FilterList()
+    public async Task FilterList(string filter)
     {
+        IsLoading = true;
         // Search Storage List Names.
         // Replace List for any matching names or semi matching.
+        var filtered = AllLobbiesStorage.Where(lobby => lobby.LobbyName.ToLower().Contains(filter.ToLower())).ToList();
+        List<LobbyBrowserPreviewControl> controls = new List<LobbyBrowserPreviewControl>();
+        for (int i = 0; i < filtered.Count; i++)
+        {
+            controls.Add(new LobbyBrowserPreviewControl()
+            {
+                LobbyName = filtered[i].LobbyName,
+                LobbyCode = filtered[i].Code,
+                CurrentSize = filtered[i].CurrentPartySize.ToString(),
+                MaxSize = filtered[i].MaxPartySize.ToString(),
+                IsPasswordProtected = filtered[i].RequiresPassword
+            });
+        }
+        CurrentlyShownLobbies = controls;
+        IsLoading = false;
+    }
+    
+    public async Task RefreshList()
+    {
+        IsLoading = true;
         
+        AllLobbiesStorage = await AssistApplication.Current.AssistUser.GetAllLobbies();
+
+        List<LobbyBrowserPreviewControl> controls = new List<LobbyBrowserPreviewControl>();
+        for (int i = 0; i < AllLobbiesStorage.Count; i++)
+        {
+            controls.Add(new LobbyBrowserPreviewControl()
+            {
+                LobbyName = AllLobbiesStorage[i].LobbyName,
+                LobbyCode = AllLobbiesStorage[i].Code,
+                CurrentSize = AllLobbiesStorage[i].CurrentPartySize.ToString(),
+                MaxSize = AllLobbiesStorage[i].MaxPartySize.ToString(),
+                IsPasswordProtected = AllLobbiesStorage[i].RequiresPassword
+            });
+        }
+        
+        CurrentlyShownLobbies = controls;
+        IsLoading = false;
     }
 }
