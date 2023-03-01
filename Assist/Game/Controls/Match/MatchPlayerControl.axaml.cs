8d7bb18a9210a3835d6bfbc866dcb5bcfd3d8a4b
Michael <49486947+HeyM1ke@users.noreply.github.com>
2023-03-01 00:36:23 -0500

    Modifications to Design

--- Assist/Game/Controls/Match/MatchPlayerControl.axaml.cs
+++ Assist/Game/Controls/Match/MatchPlayerControl.axaml.cs
@@ -6,4 +6,32 @@ namespace Assist.Game.Controls.Match;
 
 public class MatchPlayerControl : TemplatedControl
 {
+    public static readonly StyledProperty<string?> PlayerNameProperty = AvaloniaProperty.Register<MatchPlayerControl, string?>("PlayerName", "Player");
+    public static readonly StyledProperty<string?> LeaguePointTextProperty = AvaloniaProperty.Register<MatchPlayerControl, string?>("LeaguePointText", "LP: 0,000");
+    public static readonly StyledProperty<string?> ImageUrlProperty = AvaloniaProperty.Register<MatchPlayerControl, string?>("ImageUrl");
+    public static readonly StyledProperty<bool?> IsReadyProperty = AvaloniaProperty.Register<MatchPlayerControl, bool?>("IsReady", false);
+
+    public string? PlayerName
+    {
+        get { return (string?)GetValue(PlayerNameProperty); }
+        set { SetValue(PlayerNameProperty, value); }
+    }
+
+    public string? LeaguePointText
+    {
+        get { return (string?)GetValue(LeaguePointTextProperty); }
+        set { SetValue(LeaguePointTextProperty, value); }
+    }
+
+    public string? ImageUrl
+    {
+        get { return (string?)GetValue(ImageUrlProperty); }
+        set { SetValue(ImageUrlProperty, value); }
+    }
+
+    public bool? IsReady
+    {
+        get { return (bool?)GetValue(IsReadyProperty); }
+        set { SetValue(IsReadyProperty, value); }
+    }
 }
