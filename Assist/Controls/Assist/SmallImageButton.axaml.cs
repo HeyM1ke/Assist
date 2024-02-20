5795fe9e80845cbf6882307721316a7df174244b
Michael <49486947+HeyM1ke@users.noreply.github.com>
2024-02-20 18:22:52 -0500

    New Buttons for Titlebar

---
+++ Assist/Controls/Assist/SmallImageButton.axaml.cs
@@ -0,0 +1,17 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Controls.Primitives;
+using Avalonia.Media;
+
+namespace Assist.Controls.Assist;
+
+public class SmallImageButton : Button
+{
+    public static readonly StyledProperty<IImage?> IconProperty = AvaloniaProperty.Register<SmallImageButton, IImage?>("Icon");
+    
+    public IImage? Icon
+    {
+        get { return (IImage?)GetValue(IconProperty); }
+        set { SetValue(IconProperty, value); }
+    }
+}
