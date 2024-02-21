c1ae46c125cd538206ee27d1ddfcd94be35d2c96
Michael <49486947+HeyM1ke@users.noreply.github.com>
2024-02-21 17:24:16 -0500

    Basic Dodge Service Implementation

--- Assist/Services/Assist/DodgeService.cs
+++ Assist/Services/Assist/DodgeService.cs
@@ -1,6 +1,80 @@
-﻿namespace Assist.Services.Assist;
+﻿using System;
+using System.Collections.Generic;
+using System.Text.Json;
+using System.Threading.Tasks;
+using Assist.ViewModels;
+using AssistUser.Lib.V2.Models;
+using AssistUser.Lib.V2.Models.Dodge;
+using Serilog;
 
+namespace Assist.Services.Assist;
+
+/// <summary>
+/// Dodge Service Link
+/// </summary>
 public class DodgeService
 {
+    /// <summary>
+    /// Contains the List of Dodge List People Locally.
+    /// </summary>
+    private PlayerDodgeList DodgeList = new ();
+    public static DodgeService Current { get; set; }
+    public Action<UserDodgePlayer?> DodgeUserAddedToList;
+    public Action<UserDodgePlayer?> DodgeUserRemovedFromList;
     
+    public DodgeService()
+    {
+        if (Current is not null)
+            return; 
+            
+        Current = this;
+        LoadDodgeList();
+    }
+
+    private async void LoadDodgeList()
+    {
+        // First check if the user is currently signed in.
+
+        if (string.IsNullOrEmpty(AssistApplication.AssistUser.userTokens.AccessToken)) // Simple Check before real checks later.
+            return;
+        
+        try
+        {
+            Log.Information("Pinging Server to Receive Dodge List");
+
+            var resp = await AssistApplication.AssistUser.DodgeList.GetPlayerDodgeList();
+
+            if (resp.Code != 200)
+            {
+                Log.Error("Failed to receieve user dodge list.");
+                Log.Error($"Resp Data: CODE: {resp.Code}");
+                Log.Error($"Resp Data: MESSAGE: {resp.Message}");
+                return;
+            }
+
+            DodgeList = JsonSerializer.Deserialize<PlayerDodgeList>(resp.Data.ToString());
+            
+            Log.Information("Successfully read the Dodge settings file");
+        }
+        catch
+        {
+            Log.Error("Dodge Settings File was not found or tampered with.");
+        }
+    }
+
+    public async Task<UserDodgePlayer> AddPlayerToUserDodgeList(AddUserToDodgeListModel data)
+    {
+        var resp = await AssistApplication.AssistUser.DodgeList.AddPlayerToDodgeList(data);
+
+        if (resp.Code != 200)
+            throw new Exception(resp.Message);
+
+        var newUser = JsonSerializer.Deserialize<UserDodgePlayer>(resp.Data.ToString());
+        
+        DodgeList.Players.Add(newUser); // This updates the list locally without having to pull from the server.
+        
+        DodgeUserAddedToList?.Invoke(newUser); // Alerts that there is a new user added
+        
+        return newUser;
+    }
 }
